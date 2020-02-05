d85da47fc9089d37780f8a868505d0bbe453c9c3
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-02-05 15:54:37 +0100

    Rot cases, protected tarball, rename zstd -> zst

--- bin/submit.py
+++ bin/submit.py
@@ -4,11 +4,11 @@ from eturb.clusters import Cluster
 
 
 cluster = Cluster()
-name_run = "irrot"
+name_run = "rot"
 snakemake_rules = "srun"
 
 
-for nb_nodes in [1, 2]:
+for nb_nodes, walltime in zip([1, 2], ["03:00:00", "15:00:00"]):
     cmd = f"\n{sys.executable} ./simple.py -n {name_run} -w {nb_nodes} {snakemake_rules}"
 
     cluster.submit_command(
@@ -17,7 +17,7 @@ for nb_nodes in [1, 2]:
         name_run=name_run,
         # walltime='7-00:00:00',
         # walltime="06:00:00",
-        walltime="06:00:00",
+        walltime=walltime,
         ask=True,
         bash=False,
         email="avmo@misu.su.se",
