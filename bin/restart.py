671069c8347fc9bfce0747644ce5ab38967216ae
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-02-06 20:37:45 +0100

    Deploy restart script in production

--- bin/restart.py
+++ bin/restart.py
@@ -1,5 +1,6 @@
 from eturb.clusters import Cluster
 from eturb.util import prepare_for_restart
+from eturb.log import logger
 
 from fluiddyn.io import FLUIDDYN_PATH_SCRATCH
 from pathlib import Path
@@ -8,29 +9,29 @@ from pathlib import Path
 cluster = Cluster()
 name_run = "restart"
 snakemake_rules = "srun"
-dryrun = True
+dryrun = False
 
-scratch = Path(FLUIDDYN_PATH_SCRATCH)
-for path in scratch.iterdir():
+subdir = Path(FLUIDDYN_PATH_SCRATCH) / "simple"
+for path in subdir.iterdir():
     try:
         params = prepare_for_restart(path)
     except IOError as e:
-        print(e)
-        print("Skipping...")
+        logger.error(e)
+        logger.warning("Skipping...")
         continue
 
     params.nek.stat.av_step = 1
     params.nek.stat.io_step = 1
-    params.nek.general.num_steps += 500
-    params._write_par(path / "abl.par")
+    params.nek.general.num_steps = 1000
+    params.nek._write_par(path / "abl.par")
 
     #  nb_nodes = 1 if params.oper.nx <= 15 else 2
-    nb_nodes = 1 if "15x" in path else 2
-    walltime = "02:00:00"
+    nb_nodes = 1 if "15x" in path.name else 2
+    walltime = "01:00:00"
 
     cmd = f"""
 cd {path}
-mpirun {nb_nodes*cluster.nb_cores_per_node} ./nek5000 > {Path.cwd() / name_run}.${{SLURM_JOBID}}.stdout 2>&1
+mpirun {nb_nodes*cluster.nb_cores_per_node} ./nek5000 > {Path.cwd() / ("SLURM." + name_run)}.${{SLURM_JOBID}}.stdout 2>&1
 
 """
     if dryrun:
