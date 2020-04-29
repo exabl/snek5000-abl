5bcaa6fd4e20488ec7d9d5b9ee9da1c8c2faa1de
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-04-29 09:58:03 +0200

    Add print_stdout and fix restart
    The print_stdout module uses regex to parse Nek5000 log files
    
    Utility prepare_for_restart used to unconditionally fail. Now it fails only if a status >= 400 is raised
    
    Add tests for the above

--- tests/test_solver_abl.py
+++ tests/test_solver_abl.py
@@ -1,7 +1,11 @@
 import pytest
 
+from eturb.util import prepare_for_restart
+
 
 def test_init(sim):
+    print(sim.info_solver)
+    print(sim.output.print_stdout.file)
     pass
 
 
@@ -12,4 +16,14 @@ def test_make(sim):
 
 @pytest.mark.slow
 def test_make_run(sim):
-    assert sim.make.exec()
+    # Run in foreground
+    assert sim.make.exec(["srun"])
+
+    # test outputs
+    dt = sim.output.print_stdout.dt
+    # number of time steps executed, see conftest.py
+    assert dt.size == 9
+    
+    # check if simulation can be restarted
+    restart_params = prepare_for_restart(sim.path_run)
+    
