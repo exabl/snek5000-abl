ae8c9b419dd90a3efaa685d0583e594b566d3b7a
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-04-29 20:19:28 +0200

    Do not look for log file after test init

--- tests/test_solver_abl.py
+++ tests/test_solver_abl.py
@@ -5,7 +5,6 @@ from eturb.util import prepare_for_restart
 
 def test_init(sim):
     print(sim.info_solver)
-    print(sim.output.print_stdout.file)
     pass
 
 
@@ -20,10 +19,11 @@ def test_make_run(sim):
     assert sim.make.exec(["srun"])
 
     # test outputs
+    print(sim.output.print_stdout.file)
     dt = sim.output.print_stdout.dt
     # number of time steps executed, see conftest.py
     assert dt.size == 9
-    
+
     # check if simulation can be restarted
     restart_params = prepare_for_restart(sim.path_run)
-    
+
