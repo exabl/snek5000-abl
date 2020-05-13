664aa581c4980e26d271209f40798eb38be5adea
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-05-13 18:27:16 +0200

    Start implicit none, makefile_usr.inc, activate.sh
    activate.sh does not complain if virtualenv is used

--- activate.sh
+++ activate.sh
@@ -25,10 +25,9 @@ function tar-diff() {
 
 if [ -d venv ]; then
   source venv/bin/activate
-elif [ -z "$CONDA_PREFIX" ]; then
+elif [[ -z "$CONDA_PREFIX" ]] && [[ -z "$VIRTUAL_ENV" ]]; then
   echo 'WARNING: no venv / conda environment present.'
-  echo 'Read the docs on how to setup your Python environment: https://exabl.github.io/eturb/README.html#easy-way'
-fi
+  echo 'Read the docs on how to setup your Python environment: https://exabl.github.io/snek5000/README.html#installation
 
 if [ "$BASH_VERSION" ]; then
   if [ $(type -P "snakemake") ]; then
