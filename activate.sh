8937fb0b2e42a187a7ecd146089e34567536a88a
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-05-14 13:36:44 +0200

    Fix dangling if and quotes in activate.sh

--- activate.sh
+++ activate.sh
@@ -27,7 +27,8 @@ if [ -d venv ]; then
   source venv/bin/activate
 elif [[ -z "$CONDA_PREFIX" ]] && [[ -z "$VIRTUAL_ENV" ]]; then
   echo 'WARNING: no venv / conda environment present.'
-  echo 'Read the docs on how to setup your Python environment: https://exabl.github.io/snek5000/README.html#installation
+  echo 'Read the docs on how to setup your Python environment: https://exabl.github.io/snek5000/README.html#installation'
+fi
 
 if [ "$BASH_VERSION" ]; then
   if [ $(type -P "snakemake") ]; then
