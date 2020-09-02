19bfd8774658fe760df54fa7a14e6c17d90f3a59
ashwinvis <ashwinvis@users.noreply.github.com>
2020-09-02 10:15:47 +0000

    deploy: d708170b5222725089ff86f412f1b3b6b6deac58

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g1809f2e',
+    VERSION: '0.1.dev1+gd708170',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
