e4d96f7577a42cfda450f6bea7c3a521c86ee7e8
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-10-13 12:53:06 +0200

    Fix shear_imp: take norm after averaging Sij

--- src/abl/sgs/shear_imp_smag.f
+++ src/abl/sgs/shear_imp_smag.f
@@ -18,18 +18,20 @@ c> @callgraph @callergraph
 
       integer e
       real C0, delta_sq, y0
-      integer i, ie, istart, iend, ntot
+      integer i, j, k, ie, istart, iend, ntot
 
-      real snrm_planar_avg(lx1*ly1*lz1,lelv)
-      common /smag_avg/ snrm_planar_avg
+      real sij_avg(lx1*ly1*lz1,lelv,ldim,ldim)
+     &, sij_global(lx1*ly1*lz1,lelv,ldim,ldim)
+     &,   snrm_avg(lx1*ly1*lz1)
+      common /shear_imp/ sij_avg, sij_global, snrm_avg
 
       ntot = nx1*ny1*nz1
 c------need to be by element ->
-      call comp_gije(sij, vx(1,1,1,e), vy(1,1,1,e), vz(1,1,1,e), e)
+      call comp_gije(
+     &   sij_global(:,e,:,:), vx(1,1,1,e), vy(1,1,1,e), vz(1,1,1,e), e)
+      call comp_sije(sij_global(:,e,:,:))
 
-      call comp_sije(sij)
-
-      call mag_tensor_e(snrm(1,e), sij)
+      call mag_tensor_e(snrm(1,e), sij_global(:,e,:,:))
       call cmult(snrm(1,e), 2.0, ntot)
 c---------------------------
 
@@ -37,14 +39,26 @@ c------now do for every GLL point
       if (e.eq.nelv) then
         y0 = wmles_bc_z0
         C0 = wmles_sgs_c0
-        ! call planar_avg_horiz(snrm_planar_avg, snrm)
-        call planar_avg_spanwise(snrm_planar_avg, snrm)
+        do j=1,ldim
+           do k=j,ldim
+              ! call planar_avg_horiz(
+              call planar_avg_spanwise(
+     &           sij_avg(:,:,j,k), sij_global(:,:,j,k))
+              if (i .ne. j) call copy(
+     &           sij_avg(:,:,k,j), sij_avg(:,:,j,k),
+     &           lx1*ly1*lz1*lelv)
+           end do
+        end do
 
         do ie=1,nelv
           if (wmles_sgs_delta_max) delta_sq = dg2_max(ie)
 
           istart = ntot * (ie-1) + 1
           iend = ntot * ie
+
+          !> Compute norm of S_ij averaged
+          call mag_tensor_e(snrm_avg, sij_avg(:,ie,:,:))
+          call cmult(snrm_avg, 2.0, ntot)
 
           do i=istart,iend
             if (.not. wmles_sgs_delta_max) delta_sq = dg2(i,1,1,1)
@@ -53,7 +67,7 @@ c------now do for every GLL point
      $        param(2) + (
      $          (C0**2) *
      $          delta_sq *
-     $          max(snrm(i,1) - snrm_planar_avg(i,1), 0.)
+     $          max(snrm(i,1) - snrm_avg(1 + i - istart), 0.)
      $        )
      $      )
           enddo
