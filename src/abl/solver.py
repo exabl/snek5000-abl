0b01326a98376cf0e3dfc78f9abf0cbb9f296e33
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2021-01-20 04:34:26 +0100

    Remove files which are template generated, mod defaults
    Namely box, par, makefile_usr

--- src/abl/solver.py
+++ src/abl/solver.py
@@ -1,8 +1,5 @@
-from snek5000 import logger, mpi
 from snek5000.info import InfoSolverMake
 from snek5000.solvers.kth import SimulKTH
-
-from .output import OutputABL
 
 
 class InfoSolverABL(InfoSolverMake):
@@ -38,7 +35,15 @@ class SimulABL(SimulKTH):
     def _complete_params_with_default(params):
         """Add missing default parameters."""
         params = SimulKTH._complete_params_with_default(params)
-        params.nek.velocity._set_attrib("advection", True)
+
+        params.nek.problemtype.variable_properties = True
+
+        params.nek.velocity._set_attribs(
+            {
+                "advection": True,
+                "density": 1.0,
+            }
+        )
 
         params.nek._set_child(
             "wmles",
@@ -72,14 +77,6 @@ class SimulABL(SimulKTH):
 
         """
         params = super().create_default_params()
-
-        # Synchronize baseline parameters as follows:
-        # -----------------------------------------------------------------
-        primary_par_file = OutputABL.get_root() / "abl.par"
-        if mpi.rank == 0:
-            logger.info(f"Reading baseline parameters from {primary_par_file}")
-
-        params.nek._read_par(primary_par_file)
 
         return params
 
