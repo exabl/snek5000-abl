cf79df1618ca57023d45f4cc1b2987da7eb613a6
Ashwin Vishnu <y4d71nsar@relay.firefox.com>
2021-03-24 23:29:28 +0100

    Simple shell script launcher abl.sh

---
+++ bin/abl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# MESH=11; ROUGH=0.1
+# MESH=212; ROUGH=0.1
+# MESH=222; ROUGH=0.0001
+MESH=12; ROUGH=0.0001
+
+abl -d penalty -m $MESH -n penalty -o 1 -w 7-00:00:00 \
+  -fw 0.05 -fc 0.75 -ft False -sb False \
+  -s mixing_len \
+  -b noslip \
+  -zw $ROUGH -z0 $ROUGH \
+  $@
