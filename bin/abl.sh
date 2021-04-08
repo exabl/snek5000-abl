2157faaf02e08c1cfa47391a70cc2ba5b900cdbf
Ashwin Vishnu <y4d71nsar@relay.firefox.com>
2021-04-08 11:54:50 +0200

    Use channel mixing length and init penalties only if enabled

--- bin/abl.sh
+++ bin/abl.sh
@@ -6,7 +6,7 @@
 MESH=31; ROUGH=0.0001
 
 abl -d penalty -m $MESH -n penalty -o 1 -w 7-00:00:00 --in-place False \
-  -s mixing_len \
+  -s channel_mixing_len \
   -b channel \
   -zw 0.0 -z0 $ROUGH \
   -p 1.0 \
