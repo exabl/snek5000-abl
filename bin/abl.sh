01058ce4c21d34864356475d48a5bddb0082769d
Ashwin Vishnu <y4d71nsar@relay.firefox.com>
2021-04-08 12:11:42 +0200

    Change Kappa and Z0 as given by Lee Moser 2015

--- bin/abl.sh
+++ bin/abl.sh
@@ -3,7 +3,7 @@
 # MESH=212; ROUGH=0.1
 # MESH=222; ROUGH=0.0001
 # MESH=12; ROUGH=0.0001
-MESH=31; ROUGH=0.0001
+MESH=31; ROUGH="3.4289e-5"
 
 abl -d penalty -m $MESH -n penalty -o 1 -w 7-00:00:00 --in-place False \
   -s channel_mixing_len \
