77894d29c4fc03fb69003499d422ecf14b8d39cd
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2021-09-21 01:32:38 +0200

    Submit two buoy runs

--- bin/abl.sh
+++ bin/abl.sh
@@ -3,8 +3,8 @@ set -eu
 # CASE=mixing_len:with_penalty; ROUGH="0.0001"
 # CASE=mixing_len:no_penalty; ROUGH="0.0001"
 
-# CASE=buoy_test:variable_properties
-CASE=buoy_test:no_variable_properties
+CASE=buoy_test:variable_properties
+# CASE=buoy_test:no_variable_properties
 ROUGH=0.000456; Ri=0.25
 
 abl -d buoy -c $CASE -n penalty -o 1 -w 7-00:00:00 --in-place False \
--- bin/restart.py
+++ bin/restart.py
@@ -2,17 +2,17 @@
 from pathlib import Path
 
 from fluiddyn.io import FLUIDDYN_PATH_SCRATCH
-from snek5000.clusters import Cluster
+from abl.clusters import Cluster
 from snek5000.log import logger
 from snek5000.util import prepare_for_restart
 
 cluster = Cluster()
-base_name_run = "mpen"
-snakemake_rules = "srun"
+base_name_run = "penalty"
+snakemake_rules = "run_fg"
 modify_params = False
-dryrun = False
+dryrun = not True
 
-subdir = Path(FLUIDDYN_PATH_SCRATCH) / "mixing_len"
+subdir = Path(FLUIDDYN_PATH_SCRATCH) / "buoy"
 for path in filter(
     lambda path: path.name
     not in [
@@ -31,8 +31,8 @@ for path in filter(
     else:
         logger.info(f"OK {path}")
 
-    name_run = base_name_run + path.name[6:]
-    name_run = name_run[:name_run.index('_3x20x3')]
+    name_run = path.name[-8:]
+    # name_run = name_run[:name_run.index('_14x4x7')]
 
     if modify_params:
         logger.info("Modifying I/O parameters ...")
@@ -46,21 +46,21 @@ for path in filter(
 
     cmd = f"""
 cd {path}
-snakemake {snakemake_rules} -j
+snakemake {snakemake_rules} -j all
 """
-    # mpiexec -n {nb_nodes * cluster.nb_cores_per_node} ./nek5000 > abl.log
 
+# mpiexec -n {nb_nodes * cluster.nb_cores_per_node} ./nek5000 > abl.log
     if dryrun:
         if list(path.glob("rs6*")):
             logger.info(
-                "Has restart files... restart parameters will be written to abl.par"
+                "Has restart files... modified parameters will be written to abl.par"
             )
 
         print("name_run =", name_run)
         print(cmd)
     else:
         if list(path.glob("rs6*")):
-            logger.info("Has restart files... writing restart parameters to abl.par")
+            logger.info("Has restart files... writing modified parameters to abl.par")
             try:
                 params.nek._write_par(path / "abl.par")
             except NameError:
@@ -72,7 +72,7 @@ snakemake {snakemake_rules} -j
             command=cmd,
             name_run=name_run,
             # walltime="7-00:00:00",
-            walltime="1-12:00:00",
+            walltime="03:00:00",
             signal_num=False,
             ask=False,
             bash=False,
