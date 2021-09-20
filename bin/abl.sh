b0323e86792340a973f4e56cfa80e79781c10a0e
Ashwin Vishnu <y4d71nsar@relay.firefox.com>
2021-09-21 00:58:31 +0200

    Set up buoy_test parameters to match with Geert's
    Also rm duplicate default temperature parameter declaration

--- bin/abl.sh
+++ bin/abl.sh
@@ -2,11 +2,15 @@
 set -eu
 # CASE=mixing_len:with_penalty; ROUGH="0.0001"
 # CASE=mixing_len:no_penalty; ROUGH="0.0001"
-CASE=buoy_test; ROUGH=0.0
 
-abl -d mixing_len -c $CASE -n penalty -o 1 -w 7-00:00:00 --in-place False \
+# CASE=buoy_test:variable_properties
+CASE=buoy_test:no_variable_properties
+ROUGH=0.000456; Ri=0.25
+
+abl -d buoy -c $CASE -n penalty -o 1 -w 7-00:00:00 --in-place False \
   -s mixing_len \
   -b noslip \
+  -ri $Ri \
   -zw $ROUGH -z0 $ROUGH \
   -p -1e-9 \
   $*
--- src/abl/cases/buoy_test.py
+++ src/abl/cases/buoy_test.py
@@ -1,7 +1,7 @@
 """Buoyancy test case
 
 """
-from math import pi
+from math import nan, pi
 
 
 def _common(params):
@@ -18,3 +18,30 @@ def _common(params):
     z_wall = oper.origin_y
 
     oper.coords_y = f"{z_wall} 0.1 0.3 0.6 1.0"
+
+    general = params.nek.general
+    general.filter_cutoff_ratio = nan
+    general.filter_weight = 12.0
+    general.user_params.update({3: 1.0, 4: 0.0014})
+
+    reynolds_number = 5_000
+    params.nek.velocity.viscosity = -reynolds_number
+
+    temperature = params.nek.temperature
+    temperature.rho_cp = 1.0
+    temperature.conductivity = -5000.0
+
+    # Not relevant while using no-slip BC + mixing length SGS.
+    wmles = params.nek.wmles
+    wmles.bc_z_index = 12
+    wmles.sgs_c0 = 0.15
+
+
+def variable_properties(params):
+    params.nek.problemtype.variable_properties = True
+    _common(params)
+
+
+def no_variable_properties(params):
+    params.nek.problemtype.variable_properties = False
+    _common(params)
