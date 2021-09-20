bd1df5de264e7fccf56ec890433321fd2d4d4a0d
Ashwin Vishnu <y4d71nsar@relay.firefox.com>
2021-09-20 13:29:19 +0200

    Upgrade to snek5000 >= 0.7.0b0

--- tests/test_output.py
+++ tests/test_output.py
@@ -1,14 +1,16 @@
 import tempfile
 
 from abl.output import OutputABL as Output
+from snek5000.solvers import get_solver_package
 
 
 def test_copy():
     new_dir = tempfile.mkdtemp(__name__)
     output = Output()
 
-    # Inject name_solver
+    # Inject name_solver and package
     output.name_solver = "abl"
+    output.package = get_solver_package(output.name_solver)
 
     # Fresh copy
     output.copy(new_dir)
