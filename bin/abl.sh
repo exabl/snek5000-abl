00d74ae046c3d6ddc34151a3ed74fa01bdef247d
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2021-09-21 16:59:43 +0200

    Try run with Vreman SGS

--- bin/abl.sh
+++ bin/abl.sh
@@ -3,12 +3,12 @@ set -eu
 # CASE=mixing_len:with_penalty; ROUGH="0.0001"
 # CASE=mixing_len:no_penalty; ROUGH="0.0001"
 
-CASE=buoy_test:variable_properties
-# CASE=buoy_test:no_variable_properties
+# CASE=buoy_test:variable_properties
+CASE=buoy_test:no_variable_properties
 ROUGH=0.000456; Ri=0.25
 
-abl -d buoy -c $CASE -n penalty -o 1 -w 7-00:00:00 --in-place False \
-  -s mixing_len \
+abl -d buoy -c $CASE -n vreman_novp -o 1 -w 1-00:00:00 --in-place False \
+  -s vreman \
   -b noslip \
   -ri $Ri \
   -zw $ROUGH -z0 $ROUGH \
