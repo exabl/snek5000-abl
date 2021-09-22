79a1d42756fbc16e17551cfaf278765f292260ec
Ashwin Vishnu <y4d71nsar@relay.firefox.com>
2021-09-23 00:37:36 +0200

    Set constant thermal conductivity from par file

--- bin/abl.sh
+++ bin/abl.sh
@@ -3,10 +3,10 @@ set -eu
 # CASE=mixing_len:with_penalty; ROUGH="0.0001"
 # CASE=mixing_len:no_penalty; ROUGH="0.0001"
 
-# CASE=buoy_test:variable_properties
-CASE=buoy_test:no_variable_properties
 ROUGH=0.000456; Ri=0.25
 
+for CASE in buoy_test:variable_properties # buoy_test:no_variable_properties
+  do
 abl -d buoy -c $CASE -n vreman_novp -o 1 -w 1-00:00:00 --in-place False \
   -s vreman \
   -b noslip \
@@ -14,3 +14,4 @@ abl -d buoy -c $CASE -n vreman_novp -o 1 -w 1-00:00:00 --in-place False \
   -zw $ROUGH -z0 $ROUGH \
   -p -1e-9 \
   $*
+  done
