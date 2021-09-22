2ccde8e05bc4eecf66da30d0c526ad5ed5f2739a
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2021-09-23 01:32:06 +0200

    Make runs in the background

--- bin/abl.sh
+++ bin/abl.sh
@@ -7,11 +7,12 @@ ROUGH=0.000456; Ri=0.25
 
 for CASE in buoy_test:variable_properties buoy_test:no_variable_properties
   do
-abl -d buoy -c $CASE -n mixing_len -o 1 -w 1-00:00:00 --in-place False \
-  -s mixing_len \
+abl -d buoy -c $CASE -n vreman -o 1 -w 1-00:00:00 --in-place False \
+  -s vreman \
   -b noslip \
   -ri $Ri \
   -zw $ROUGH -z0 $ROUGH \
   -p -1e-9 \
-  $*
+  $* &
+sleep 5
   done
