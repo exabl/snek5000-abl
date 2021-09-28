6752337f51f87dcd37ff19b37732a0a1e44e9519
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2021-09-28 11:34:44 +0200

    buoy_ic runs

--- bin/abl.sh
+++ bin/abl.sh
@@ -3,12 +3,12 @@ set -eu
 # CASE=mixing_len:with_penalty; ROUGH="0.0001"
 # CASE=mixing_len:no_penalty; ROUGH="0.0001"
 
-ROUGH=0.000456; Ri=0.25
+ROUGH=0.0; Ri=0.25
 
 for CASE in buoy_test:variable_properties buoy_test:no_variable_properties
   do
-abl -d buoy -c $CASE -n vreman -o 1 -w 1-00:00:00 --in-place False \
-  -s vreman \
+abl -d buoy_ic -c $CASE -n mixing_len -o 1 -w 1-00:00:00 --in-place False \
+  -s mixing_len \
   -b noslip \
   -ri $Ri \
   -zw $ROUGH -z0 $ROUGH \
