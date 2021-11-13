36ed486326c6f2368fc8cea480d1792cb357d056
Ashwin V. Mohanan <dev@fluid.quest>
2021-11-14 00:27:24 +0100

    Set up cases

--- bin/abl.sh
+++ bin/abl.sh
@@ -3,10 +3,11 @@ set -eu
 # CASE=mixing_len:with_penalty; ROUGH="0.0001"
 # CASE=mixing_len:no_penalty; ROUGH="0.0001"
 
-ROUGH=0.0; Ri=0.01
+ROUGH=0.000456
+SPONGE_STRENGTH=0.0
 
 for CASE in buoy_test:no_vp_sponge; do
-  for SPONGE_STRENGTH in 0.0; do
+  for Ri in 0.0 0.01; do
     #"0.01" "0.1" "1.0" "10.0"; do
 abl -d buoy_test_sponge -c $CASE -n sponge -o 2 -w 1-00:00:00 --in-place False \
   -s mixing_len \
